fb19b5b903ffad78c7c9390b96ae035701d30094
ashawkey <963113090@qq.com>
2020-08-14 20:43:49 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
-import heart from "./heart.png"
+import white_heart from "./white.png"
 
 function App() {
   return (
     <div className="App">
-      <div className="header"> 
-        Hawome &
-        <hr className="fancy-line" />
-      </div>
-      <div className="content">
-        Seeking consolation of meaning in a mad song, <br/> 
-        walking alone on the bright road.<br/>
-        <a href="https://ashawkey.github.io/blogs"> <img className="heart" src={heart} alt="heart"/> </a>
-      </div>
-      <div className="footer">
-        <hr className="fancy-line" />
-        @ hawkey
+      <div className="centered">
+        <a href="https://ashawkey.github.io/blogs"> <img className="heart" src={white_heart} alt="heart"/> </a>
       </div>
     </div>
   );
