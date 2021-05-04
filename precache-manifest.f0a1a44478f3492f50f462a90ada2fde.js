c4ddb13b99bef50702b4248714ac3c1fe2aa4fe4
ashawkey <ashawkey1999@gmail.com>
2021-05-04 11:04:39 +0800

    Updates

---
+++ precache-manifest.f0a1a44478f3492f50f462a90ada2fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7ba5fb3144e5a2f1aff02d4bc267aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f7e10a8b642ab5b407",
+    "url": "/static/css/main.46de4d4c.chunk.css"
+  },
+  {
+    "revision": "e9865bae08b452962746",
+    "url": "/static/js/2.c7a64eb2.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/static/js/2.c7a64eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f7e10a8b642ab5b407",
+    "url": "/static/js/main.940ad8b9.chunk.js"
+  },
+  {
+    "revision": "e09751e01fd2c52ac4a8",
+    "url": "/static/js/runtime-main.5ffb660f.js"
+  }
+]);
